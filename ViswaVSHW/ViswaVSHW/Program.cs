fda98aa7de6ab7f5d267450552907256f450382c
viswa anand <urviswaanand@gmail.com>
2018-01-27 09:29:27 -0800

    Second change to verify

--- ViswaVSHW/ViswaVSHW/Program.cs
+++ ViswaVSHW/ViswaVSHW/Program.cs
@@ -7,6 +7,8 @@ namespace ViswaVSHW
         static void Main(string[] args)
         {
             //I am viswaanand before change 
+
+            // The changes after the change sync the github
         }
     }
 }
